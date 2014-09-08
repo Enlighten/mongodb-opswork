e134be80fc92b75d1858757ff3b0c52b8f79c872
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-08 13:33:51 -0700

    Another attempt at bind_ip.

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,3 +1,3 @@
-node.set['mongodb']['config']['bind_ip'] = '0.0.0.0'
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
+node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
