0b76072f518ce2b3c39798b9d7e7cbf66845ccb2
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-09 16:01:01 -0700

    .

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -2,3 +2,7 @@ node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
 
 include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::default"
+
+service "mongod" do
+	action [:stop, :disable]
+end
