9614188fe8c2ac33c0172a479faa4b59d87b3abe
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-09 14:39:45 -0700

    .

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,7 +1,4 @@
+node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
+
 include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::default"
-#node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
-mongodb_instance "mongodb1" do
-  port 27018
-  bind_ip "0.0.0.0"
-end
