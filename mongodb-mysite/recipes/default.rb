befee447e3cc0f04581260ac9753775fb2a76a96
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-09 11:22:33 -0700

    more config changes

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,3 +1,6 @@
 include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::default"
-node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
+#node.normal['mongodb']['config']['bind_ip'] = "0.0.0.0"
+mongodb_instance "mongodb" do
+  bind_ip 0.0.0.0
+end
