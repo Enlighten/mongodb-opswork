cf5a68195e242fc60a6e11e5e51ed7943a3c8112
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-09 19:18:42 -0700

    .

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,11 +1,11 @@
 include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::default"
 
-node.default['mongodb']['config']['bind_ip'] = "0.0.0.0"
-node.default['mongodb']['dbconfig_file'] = '/etc/mongod.conf'
-node.default['mongodb']['config']['logpath'] = '/var/log/mongo/mongod.log'
-node.default['mongodb']['config']['dbpath'] = '/var/lib/mongo'
-node.default['mongodb']['sysconfig_file'] = '/etc/default/mongod'
-node.default['mongodb']['default_init_name'] = 'mongod'
-node.default['mongodb']['instance_name'] = 'mongod'
+node.set['mongodb']['config']['bind_ip'] = "0.0.0.0"
+node.set['mongodb']['dbconfig_file'] = '/etc/mongod.conf'
+node.set['mongodb']['config']['logpath'] = '/var/log/mongo/mongod.log'
+node.set['mongodb']['config']['dbpath'] = '/var/lib/mongo'
+node.set['mongodb']['sysconfig_file'] = '/etc/default/mongod'
+node.set['mongodb']['default_init_name'] = 'mongod'
+node.set['mongodb']['instance_name'] = 'mongod'
 
