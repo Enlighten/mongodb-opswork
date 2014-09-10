df5f5b8bdb9f741af1a905b9b73c8e3be8b9b074
Eric Mickelson <emickelson@enphaseenergy.com>
2014-09-10 10:19:49 -0700

    .

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,6 +1,3 @@
-include_recipe "mongodb::mongodb_org_repo"
-include_recipe "mongodb::default"
-
 node.set['mongodb']['config']['bind_ip'] = "0.0.0.0"
 node.set['mongodb']['dbconfig_file'] = '/etc/mongod.conf'
 node.set['mongodb']['config']['logpath'] = '/var/log/mongo/mongod.log'
@@ -8,4 +5,7 @@ node.set['mongodb']['config']['dbpath'] = '/var/lib/mongo'
 node.set['mongodb']['sysconfig_file'] = '/etc/default/mongod'
 node.set['mongodb']['default_init_name'] = 'mongod'
 node.set['mongodb']['instance_name'] = 'mongod'
+
+include_recipe "mongodb::mongodb_org_repo"
+include_recipe "mongodb::default"
 
